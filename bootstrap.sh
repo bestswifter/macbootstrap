5e87e06bcbde02b34d9b87af0485688f1cfd045d
bestswifter <ktzhang@bestswifter.com>
2017-03-31 14:03:02 +0800

    Install source tree

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew cask install iterm2
-
+brew cask install sourcetree
 brew install wget
 
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
