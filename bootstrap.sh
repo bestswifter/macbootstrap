1d6fb54c170bb282019b748a84dbb2f0c656ad64
bestswifter <ktzhang@bestswifter.com>
2017-03-31 14:08:42 +0800

    Install MacVim. Use zip because homebrew doesn’t works well now

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,5 +6,9 @@ brew cask install iterm2
 brew cask install sourcetree
 brew install wget
 
+# install MacVim
+unzip software/MacVim.zip
+cp -r MacVim.app /Applications
+
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 
