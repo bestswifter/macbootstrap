084ed7a695b5264b2cf4d6e7a2d3ed80a62f4dc0
bestswifter <ktzhang@bestswifter.com>
2017-03-31 13:26:07 +0800

    oy-my-zsh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,3 +3,5 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew cask install iterm2
+
+curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
