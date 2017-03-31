3c0cee0efa60f374dda7927a9aecf20fae25f5a0
bestswifter <ktzhang@bestswifter.com>
2017-03-31 14:39:47 +0800

    Link git config

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew cask install iterm2
@@ -11,6 +12,10 @@ unzip software/MacVim.zip
 mv MacVim.app /Applications/MacVim.app
 ln -s /Applications/MacVim.app/Contents/bin/gvim /usr/local/bin/gvim
 rm -rf __MACOSX
+
+# link git config
+mv ~/.gitconfig ~/.gitconfig_back
+ln -s git-config/.gitconfig ~/.gitconfig
 
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 
