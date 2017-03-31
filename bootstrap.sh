dba834ef280124737c457aa83deb813be61ff77a
bestswifter <ktzhang@bestswifter.com>
2017-03-31 14:35:48 +0800

    Install Macvim

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,9 @@ brew install wget
 
 # install MacVim
 unzip software/MacVim.zip
-cp -r MacVim.app /Applications
+mv MacVim.app /Applications/MacVim.app
+ln -s /Applications/MacVim.app/Contents/bin/gvim /usr/local/bin/gvim
+rm -rf __MACOSX
 
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 
