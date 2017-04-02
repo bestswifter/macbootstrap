bf4ab9f6e9d102dc4b6fd9e581f3e84884dfa447
bestswifter <ktzhang@bestswifter.com>
2017-04-02 10:22:59 +0800

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,1 +1,14 @@
 rm -rf ~/.macbootstrap/
+rm -rf /Applications/MacVim.app/
+rm -f /usr/local/bin/gvim
+
+brew cask uninstall iterm2
+brew cask uninstall sourcetree
+brew uninstall wget
+brew uninstall gnu-sed
+brew uninstall autojump
+
+rm -f ~/.zshrc
+rm -f ~/.vimrc
+rm -f ~/.gitconfig
+rm -rf ~/.vim
