6de79d4ad4a742ed69af248be844aa1e64b8d98c
zhangxingyu <zhangxingyu@baidu.com>
2017-04-14 16:36:41 +0800

    Feature：新增函数 bsctags，封装 ctags 的操作，在当前文件夹下生成 tags，忽略 .git

--- zsh-config/personalized.sh
+++ zsh-config/personalized.sh
@@ -3,3 +3,8 @@ function bsfn () {
     # Use -or to support multi types
     find . \( -type f -or -type l \) | egrep --color=always $1
 }
+
+# Make ctags in current directory
+function bsctags() {
+    `brew --prefix`/bin/ctags --exclude='.git' -R . &> /dev/null
+}
