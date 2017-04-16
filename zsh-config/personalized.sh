0e979148a08b497cdd847f19babeb215b68aa7f7
zhangxingyu <zhangxingyu@baidu.com>
2017-04-16 10:54:09 +0800

    update ctags command

--- zsh-config/personalized.sh
+++ zsh-config/personalized.sh
@@ -5,6 +5,15 @@ function bsfn () {
 }
 
 # Make ctags in current directory
-function bsctags() {
-    `brew --prefix`/bin/ctags --exclude='.git' -R . &> /dev/null
+function objctags() {
+    ctags --exclude='.git' --exclude='*.js' \
+    --langmap=objc:.m.h \
+    --totals=yes \
+    --tag-relative=yes \
+    --regex-objc='/^[[:space:]]*[-+][[:space:]]*\([[:alpha:]]+[[:space:]]*\*?\)[[:space:]]*([[:alnum:]]+):[[:space:]]*\(/\1/m,method/' \
+    --regex-objc='/^[[:space:]]*[-+][[:space:]]*\([[:alpha:]]+[[:space:]]*\*?\)[[:space:]]*([[:alnum:]]+)[[:space:]]*\{/\1/m,method/' \
+    --regex-objc='/^[[:space:]]*[-+][[:space:]]*\([[:alpha:]]+[[:space:]]*\*?\)[[:space:]]*([[:alnum:]]+)[[:space:]]*\;/\1/m,method/' \
+    --regex-objc='/^[[:space:]]*\@property[[:space:]]+.*[[:space:]]+\*?(.*);$/\1/p,property/' \
+    --regex-objc='/^[[:space:]]*\@implementation[[:space:]]+(.*)$/\1/c,class/' \
+    --regex-objc='/^[[:space:]]*\@interface[[:space:]]+(.*)[[:space:]]+:.*{/\1/i,interface/' -R .&> /dev/null
 }
