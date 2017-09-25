521512da0780e6682a168abdfe8d6429aad725f2
zhangxingyu <zhangxingyu@baidu.com>
2017-09-25 18:07:28 +0800

    Format output of proxy help

--- zsh-config/platform.mac.sh
+++ zsh-config/platform.mac.sh
@@ -64,11 +64,11 @@ function proxy() {
         ;;
     *)
         echo "Usage: p {on|off|g|s}"
-        echo "p on: Set proxy to Charles(port 8888)"
+        echo "p on : Set proxy to Charles(port 8888)"
         echo "p off: Reset proxy to system default"
-        echo "p g: Set proxy to GoAgentx(port 14179)"
-        echo "p s: Show current network proxy status"
-        echo "p *: Show usage"
+        echo "p g  : Set proxy to GoAgentx(port 14179)"
+        echo "p s  : Show current network proxy status"
+        echo "p *  : Show usage"
         ;;
     esac
 }
