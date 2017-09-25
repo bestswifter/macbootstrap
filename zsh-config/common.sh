e424ca84dece9fc205645020f912b419bf8f973b
zhangxingyu <zhangxingyu@baidu.com>
2017-09-25 17:46:10 +0800

    Add shell colors

--- zsh-config/common.sh
+++ zsh-config/common.sh
@@ -10,5 +10,4 @@ source $BS_ZSH_BASE/zsh-config/tools.sh
 # Script only for mac
 source $BS_ZSH_BASE/zsh-config/platform.mac.sh
 source $BS_ZSH_BASE/zsh-config/personalized.sh
-
-
+source $BS_ZSH_BASE/zsh-config/colors.sh
