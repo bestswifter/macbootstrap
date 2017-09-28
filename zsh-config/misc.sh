456381337d004bcdd3179fd12eedd5eb51f1d59a
zhangxingyu <zhangxingyu@baidu.com>
2017-09-28 13:15:48 +0800

    Update row function

--- zsh-config/misc.sh
+++ zsh-config/misc.sh
@@ -33,8 +33,39 @@ function column() {
     esac
 }
 
-function line() {
-    awk -v l=$1 'NR == l'
+function row() {
+    case $# in
+        1)
+            awk -v c1=$1 'NR == c1'
+        ;;
+        2)
+            awk -v c1=$1 -v c2=$2 'NR == c1 || NR == c2'
+        ;;
+        3)
+            awk -v c1=$1 -v c2=$2 -v c3=$3 'NR == c1 || NR == c2 || NR == c3'
+        ;;
+        4)
+            awk -v c1=$1 -v c2=$2 -v c3=$3 -v c4=$4 'NR == c1 || NR == c2 || NR == c3 || NR == c4'
+        ;;
+        5)
+            awk -v c1=$1 -v c2=$2 -v c3=$3 -v c4=$4 -v c5=$5 'NR == c1 || NR == c2 || NR == c3 || NR == c4 || NR == c5'
+        ;;
+        6)
+            awk -v c1=$1 -v c2=$2 -v c3=$3 -v c4=$4 -v c5=$5 -v c6=$6 'NR == c1 || NR == c2 || NR == c3 || NR == c4 || NR == c5 || NR == c6'
+        ;;
+        7)
+            awk -v c1=$1 -v c2=$2 -v c3=$3 -v c4=$4 -v c5=$5 -v c6=$6 -v c7=$7 'NR == c1 || NR == c2 || NR == c3 || NR == c4 || NR == c5 || NR == c6 || NR == c7'
+        ;;
+        8)
+            awk -v c1=$1 -v c2=$2 -v c3=$3 -v c4=$4 -v c5=$5 -v c6=$6 -v c7=$7 -v c8=$8 'NR == c1 || NR == c2 || NR == c3 || NR == c4 || NR == c5 || NR == c6 || NR == c7 || NR == c8'
+        ;;
+        9)
+            awk -v c1=$1 -v c2=$2 -v c3=$3 -v c4=$4 -v c5=$5 -v c6=$6 -v c7=$7 -v c8=$8 -v c9=$9 'NR == c1 || NR == c2 || NR == c3 || NR == c4 || NR == c5 || NR == c6 || NR == c7 || NR == c8 || NR == c9'
+        ;;
+        *)
+            echo "Usage: row [row ...]"
+        ;;
+    esac
 }
 
 function add() {
