54615db27fdf0071cc97a34e992190d5daffd5ba
bestswifter <ktzhang@bestswifter.com>
2017-10-09 17:48:52 +0800

    download youtube video

--- zsh-config/personalized.sh
+++ zsh-config/personalized.sh
@@ -4,6 +4,15 @@ function bsfn () {
     find . \( -type f -or -type l \) | egrep --color=always $1
 }
 
+# Download youtube video to ~/Movies/Youtube
+function y () {
+    if [[ ! -e /usr/local/bin/youtube-dl  ]]; then
+        brew install youtube-dl
+    fi
+
+    youtube-dl --proxy socks5://127.0.0.1:14179/ --console-title $1 -f 'best' -o '~/Movies/Youtube/%(title)s.%(ext)s'
+}
+
 # Make ctags in current directory
 function objctags() {
     ctags --exclude='.git' --exclude='*.js' \
