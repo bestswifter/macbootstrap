95831590c82d4aa366c1255782b81f3442a0d5ff
zhangxingyu <zhangxingyu@baidu.com>
2017-12-16 16:05:00 +0800

    查看远程仓库的分支

--- zsh-config/alias.sh
+++ zsh-config/alias.sh
@@ -4,6 +4,7 @@ alias gcn='git config user.name'
 alias gcng='git config --global user.name'
 alias gce='git config user.email'
 alias gceg='git config --global user.email'
+alias grsh='git remote show'
 
 # zsh
 alias showFiles='defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder'
