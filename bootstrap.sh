9453cf70a8a56bf7597f8a09e487a892671eec7d
Kt Zhang <649395594@qq.com>
2018-01-12 11:41:16 +0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,8 @@
 #!bin/bash
+if [[ -e ~/.macbootstrap ]]; then
+  rm -rf ~/.macbootstrap
+fi
+
 git clone https://github.com/bestswifter/macbootstrap.git ~/.macbootstrap
 cd ~/.macbootstrap
 bash install.sh
