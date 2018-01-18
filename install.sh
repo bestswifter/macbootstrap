e0d642b12256f7d4fd833f00a6ba0e540ac6a5d7
Kt Zhang <649395594@qq.com>
2018-01-18 14:19:32 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -80,7 +80,7 @@ ln -s ~/.macbootstrap/zsh-config/.zshrc ~/.zshrc
 # vim configuration
 backup_file ~/.vim
 backup_file ~/.config/nvim/
-git clone git://github.com/bestswifter/vim-config.git ~/.config/nvim
+git clone https://github.com/bestswifter/vim-config.git ~/.config/nvim
 ln -s ~/.vim ~/.config/nvim
 
 unset ALL_PROXY
