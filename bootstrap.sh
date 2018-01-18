fe033ebb3489d4f0afdacc706c8456601e4c425c
Kt Zhang <649395594@qq.com>
2018-01-18 10:20:53 +0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,14 @@ if [[ -e ~/.macbootstrap ]]; then
   rm -rf ~/.macbootstrap
 fi
 
+if [[ ! -e /usr/local/bin/brew ]]; then
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+    echo "You have installed brew"
+fi
+
+brew install git
+
 git clone https://github.com/bestswifter/macbootstrap.git ~/.macbootstrap
 cd ~/.macbootstrap
 bash install.sh
