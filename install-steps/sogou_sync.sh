74442b61efe8786e32b747da9dcb564a36f95871
bestswifter <ktzhang@bestswifter.com>
2018-01-20 17:10:15 +0800

    [Basic]优化同步搜狗输入法配置的逻辑

--- install-steps/sogou_sync.sh
+++ install-steps/sogou_sync.sh
@@ -1,5 +1,7 @@
 # Sync Sougou Config
 cp config/SogouPreferences.plist config/UserPreferences.plist
 mv config/UserPreferences.plist "$HOME/Library/Application Support/Sogou/InputMethod/SogouPY/"
+
+cp config/sogouUserSet.plist config/autoEngUserSet.plist
 mv config/autoEngUserSet.plist "$HOME/Library/Application Support/Sogou/InputMethod/SogouPY/"
 
