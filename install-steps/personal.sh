9f598c2a0cbfd9e9eefb3b207ff1463c0fad36ba
bestswifter <ktzhang@bestswifter.com>
2018-05-14 09:14:44 +0800

    [Feature] Install MindNode

--- install-steps/personal.sh
+++ install-steps/personal.sh
@@ -6,6 +6,7 @@ if [[ "$username" == $(whoami) ]]; then
     git config --global user.name bestswifter
     git config --global user.email ktzhang@bestswifter.com
 
+    # ssh key
     if [[ ! -f $HOME/.ssh/id_rsa ]]; then
         ln -s $HOME/.macbootstrap/config/id_rsa $HOME/.ssh/
     fi
@@ -15,4 +16,16 @@ if [[ "$username" == $(whoami) ]]; then
     fi
 
     ssh-add -K $HOME/.ssh/id_rsa
+
+    # Install MindNode
+    if [[ -e /Applications/MindNode\ 2.app ]]; then
+        echo "You have installed MindNode"
+    else
+        if [[ ! -e $HOME/Downloads/MindNode.app.zip ]]; then
+            curl "http://app.bestswifter.com/MindNode501.app.zip" -o ~/Downloads/MindNode.app.zip
+        fi
+
+        unzip -q $HOME/Downloads/MindNode.app.zip -d /Applications/MindNode\ 2.app
+        rm $HOME/Downloads/MindNode.app.zip
+    fi
 fi
