dfe1e00642da98faae77f6a7a1d7459368aebbed
zhangxingyu.bs <zhangxingyu.bs@bytedance.com>
2018-12-17 22:10:11 +0800

    不换键位

--- onlogin.sh
+++
@@ -1,3 +0,0 @@
-hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x7000000E0},{"HIDKeyboardModifierMappingSrc":0x7000000E0,"HIDKeyboardModifierMappingDst":0x700000039}]}'
-
-bssclient
