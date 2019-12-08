529818b97a03417f976ae2ba80d48d881cbe503a
Kt Zhang <ktzhang@bestswifter.com>
2019-12-07 19:32:52 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ fi
 
 # install and use shadowsocks
 if not_tt_network; then
-    nohup sslocal -q -c ~/.macbootstrap/tools/netconf &> /private/tmp/nohup.out&
+    nohup sslocal -c ~/.macbootstrap/tools/netconf &> /private/tmp/nohup.out&
     export ALL_PROXY=socks5://127.0.0.1:14179
 else
     echo "You are in toutiao network, no need to use ss now"
